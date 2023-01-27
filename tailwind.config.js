802698807069d6eaa6bff4d2477ecfdfab4ed3b1
entering-a-username <tamunacer@gmail.com>
2023-01-27 18:27:29 +0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "script.js"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "backg": "url('https://24.media.tumblr.com/c7509c344a1d3297fe4fd068d185a2ea/tumblr_n0meznL8ES1rdk4zmo1_1280.jpg')",
+        "navbackg": "url('https://64.media.tumblr.com/cafa9d8f3d80be58e41be54dac648b76/tumblr_mq0vqjjSfs1rdk4zmo1_500.png')",
+      }
+    },
+    
+  },
+  plugins: [],
+}
